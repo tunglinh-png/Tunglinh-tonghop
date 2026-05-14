2877f0604927334f39f4ed5d607272df12fab18d
tunglinh-png <tunglinhla91@gmail.com>
2026-05-14 18:39:10 +0700

    Create Scriptronghop_tunglinh.lua

---
+++ Scriptronghop_tunglinh.lua
@@ -0,0 +1,122 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Tung Linh dz",
+         Animation = "Youtube: Tung linh"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=1838109335",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     local Tab2o = MakeTab({Name ="script tieng Viet"})
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "longhihi",
+    Callback = function()
+	  loadstring(game:HttpGet(" https://raw.githubusercontent.com/longhihilonghihi-hub/Devs-LongHiHiV2/refs/heads/main/MainV2.txt"))()
+	  end
+	  })
+	  AddButton(Tab1o, {
+     Name = "banana fake",
+    Callback = function()
+    repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+
+getgenv().Key = "XyZpQrStUvWxYzAbCdEfGhIjKl" 
+
+-- nanaontop
+
+-- NaNa mãi đỉnh
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuemmm/vthangsiitink/refs/heads/main/BananaHub.lua"))()
+end
+})
+AddButton(Tab1o, {
+     Name = "ok hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fakekuri/Okhubhere/refs/heads/main/MainBloxFruit.lua"))()
+    end
+  }) 
+AddButton(Tab1o, {
+     Name = "min hub có tiếng việt",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trungdao2k4/buffalo/refs/heads/main/trauhubv10"))()
+    end
+  })
+AddButton(Tab1o, {
+     Name = "starrymoon hub",
+    Callback = function()
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+getgenv().Team = "Marines" -- Can change Marines => Pirates
+loadstring(game:HttpGet("https://power-clock-api.vercel.app/api/clockbloxfruits", true))()
+    end
+  })
+AddButton(Tab1o, {
+     Name = "yuri hub",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jadelly261/BloxFruits/main/YuriMain", true))()
+    end
+  })
+AddButton(Tab1o, {
+     Name = "shinichi hub",
+    Callback = function()
+      repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shinichi-dz/phucshinsayhi/refs/heads/main/ShinichiHub.lua"))()
+    end
+  })
+AddButton(Tab2o, {
+     Name = "trẩu v8",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/trungdao2k4/buffalo/refs/heads/main/trauhubv10"))()
+    end
+  })
+AddButton(Tab2o, {
+     Name = "tunglinh fix lag",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/tunglinh-png/Tunglinh_boost_fps.lua/refs/heads/main/Tunglinh%20t%C4%83ng%20t%E1%BB%91c%20fps.lua"))()
+    end
+  })
+AddButton(Tab2o, {
+     Name = "tunglinh fix lag v3",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/tunglinh-png/Tunglinh_boost_fps.lua/refs/heads/main/Tunglinh.lua"))()
+    end
+  })
+AddButton(Tab2o, {
+     Name = "maru hub nokey",
+    Callback = function()
+      getgenv().Team = "Marines"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/KimP/refs/heads/main/MaruHub"))()
+    end
+  })
+AddButton(Tab2o, {
+     Name = "trái cam hub",
+    Callback = function()
+      loadstring(game: HttpGet("https://raw.githubusercontent.com/HieuDepTrai-Z/Dev_Orange/refs/heads/main/OrangeHub.lua"))()
+    end
+  })
